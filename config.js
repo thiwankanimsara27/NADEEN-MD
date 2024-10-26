9819fffb02dfcad2e3ee4ef2d33d5ba363f805c9
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-26 11:26:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", //put your session id
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/l7e7kt.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
