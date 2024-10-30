e61a0a64534d611cdf8121cbae0febf0463d7563
thiwankanimsara27 <thiwankanimsara27@gmail.com>
2024-10-30 21:05:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9nVWEBiB#eQDldO9JPmiiDy_1zafEGQ1v1j5aRQKPKn_jHPCJibI", //put your session id
+SESSION_ID: process.env.SESSION_ID || "KNUj3JxR#mrpv9EXCyTRWMJdQV-PLLb7Q3or79GFZqE-fbbzCXns", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am thiwanka nimsara*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
 AUTO_VOICE:"true" //true or false
